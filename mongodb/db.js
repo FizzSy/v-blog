ba2d25a00fe497764003a0b93a8a0534704096ca
lu <1369530215@qq.com>
2018-03-07 23:40:48 +0800

    news

--- mongodb/db.js
+++ mongodb/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://172.16.6.84:27017/mydb'); //192.168.1.111 172.16.6.61
+mongoose.connect('mongodb://192.168.1.102:27017/mydb'); //192.168.1.111 172.16.6.61
 mongoose.connection.on('connected',function(){
 	console.log('数据库连接成功...');
 });
